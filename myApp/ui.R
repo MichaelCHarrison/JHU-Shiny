8072726733d4c97949f0f3717f2d4ec7ed084584
MichaelCHarrison <michael.harrison2010@gmail.com>
2017-07-10 14:14:03 -0700

    Superficial changes; insignificant

--- myApp/ui.R
+++ myApp/ui.R
@@ -1,13 +1,16 @@
 library(shiny)
 
 shinyUI(fluidPage(
-  titlePanel("Data Science, Testing 1, 2, 3"),
-  sidebarLayout(
-    sidebarPanel(
-            h3("Sidebar Text")
-    ),
-    mainPanel(
-       h3("Main Panel Text")
-    )
-  )
+        titlePanel("Data Science, Testing 1, 2, 3"),
+        sidebarLayout(
+                sidebarPanel(
+                        h1("H1 Text"),
+                        h2("H2 Text"),
+                        h3("H3 Text"),
+                        em("emphasized text")
+                ),
+        mainPanel(
+                h3("Main Panel Text")
+        )
+)
 ))
